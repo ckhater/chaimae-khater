b82dcb7cc0038eaf4e28242fccd40e1e24ee24af
ckhater <chaymaekhater13@gmail.com>
2026-09-09 02:35:00 +1200

    trying

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={firacode.className}>
         <ThemeProvider >
-        <div className="pointer-events-none fixed inset-0 z-[-1] h-[100dvh] w-screen">
+        <div className="pointer-events-none fixed inset-0 min-h-full w-full">
 <Grainient
                   timeSpeed={0.4}
                   contrast={0.95}
